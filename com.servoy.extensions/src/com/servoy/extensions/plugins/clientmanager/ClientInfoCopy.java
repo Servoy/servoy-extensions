6a3cf2bf687204d793133f32cfe431e3b37e5eec
Johan Compagner <jcompagner@servoy.com>
2018-01-16 11:43:32 +0100

    SVY-11983 clientmanager.getConnectedClients() Not Working on Server
    made a copy class of the client information object. so that the
    ClientInformation class of the server project is never send/given to a
    client. This makes all state really a copy (also last idle and the other
    dates)

---
+++ com.servoy.extensions/src/com/servoy/extensions/plugins/clientmanager/ClientInfoCopy.java
@@ -0,0 +1,133 @@
+/*
+ This file belongs to the Servoy development and deployment environment, Copyright (C) 1997-2018 Servoy BV
+
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Affero General Public License as published by the Free
+ Software Foundation; either version 3 of the License, or (at your option) any
+ later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License along
+ with this program; if not, see http://www.gnu.org/licenses or write to the Free
+ Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+*/
+
+package com.servoy.extensions.plugins.clientmanager;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.servoy.j2db.server.shared.IClientInformation;
+
+/**
+ * @author jcomp
+ *
+ */
+public class ClientInfoCopy implements IClientInformation, Serializable
+{
+	private final String clientID;
+	private final String hostIdentifier;
+	private final String hostName;
+	private final String hostAddress;
+	private final String userName;
+	private final String userUID;
+	private final int applicationType;
+	private final String openSolutionName;
+	private final Date lastAccessedTime;
+	private final Date idleTime;
+	private final Date loginTime;
+	private final String[] clientInfos;
+
+	public ClientInfoCopy(IClientInformation info)
+	{
+		clientID = info.getClientID();
+		hostIdentifier = info.getHostIdentifier();
+		hostName = info.getHostName();
+		hostAddress = info.getHostAddress();
+		userName = info.getUserName();
+		userUID = info.getUserUID();
+		applicationType = info.getApplicationType();
+		openSolutionName = info.getOpenSolutionName();
+		lastAccessedTime = info.getLastAccessedTime();
+		idleTime = info.getIdleTime();
+		loginTime = info.getLoginTime();
+		clientInfos = info.getClientInfos();
+
+	}
+
+	@Override
+	public String getClientID()
+	{
+		return clientID;
+	}
+
+	@Override
+	public String getHostIdentifier()
+	{
+		return hostIdentifier;
+	}
+
+	@Override
+	public String getHostName()
+	{
+		return hostName;
+	}
+
+	@Override
+	public String getHostAddress()
+	{
+		return hostAddress;
+	}
+
+	@Override
+	public int getApplicationType()
+	{
+		return applicationType;
+	}
+
+	@Override
+	public String getUserUID()
+	{
+		return userUID;
+	}
+
+	@Override
+	public String getUserName()
+	{
+		return userName;
+	}
+
+	@Override
+	public Date getLoginTime()
+	{
+		return loginTime;
+	}
+
+	@Override
+	public Date getIdleTime()
+	{
+		return idleTime;
+	}
+
+	@Override
+	public String getOpenSolutionName()
+	{
+		return openSolutionName;
+	}
+
+	@Override
+	public Date getLastAccessedTime()
+	{
+		return lastAccessedTime;
+	}
+
+	@Override
+	public String[] getClientInfos()
+	{
+		return clientInfos;
+	}
+
+}
