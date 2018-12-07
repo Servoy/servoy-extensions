02f8ea57fe1140279a151f60b7925918e21ab1d3
lvostinar <lvostinar@servoy.com>
2018-12-07 16:40:03 +0200

    SVY-13070 plugins.window.showFormPopup

--- com.servoy.extensions/src/com/servoy/extensions/plugins/window/popupform/FormPopup.java
+++ com.servoy.extensions/src/com/servoy/extensions/plugins/window/popupform/FormPopup.java
@@ -21,6 +21,7 @@ import com.servoy.base.scripting.annotations.ServoyClientSupport;
 import com.servoy.j2db.documentation.ServoyDocumented;
 import com.servoy.j2db.scripting.IJavaScriptType;
 import com.servoy.j2db.scripting.IScriptable;
+import com.servoy.j2db.ui.IComponent;
 
 /**
  * @author lvostinar
@@ -31,52 +32,232 @@ import com.servoy.j2db.scripting.IScriptable;
 public class FormPopup implements IScriptable, IJavaScriptType
 {
 
+	/**
+	 * Get the component form popup will be shown relative to.
+	 *
+	 * @sample
+	 * popupform.component();
+	 *
+	 * @return IComponent
+	 */
+	public IComponent js_component()
+	{
+		return null;
+	}
+
+	/**
+	 * Set component form popup will be shown relative to. If null, will use coordinates or show at screen center.
+	 *
+	 * @sample
+	 * plugins.window.createFormPopup(forms.orderPicker).component(elements.myelement).show();
+	 *
+	 * @param component the form to show
+	 *
+	 */
+	public void js_component(IComponent component)
+	{
+
+	}
+
+	/**
+	 * Get the popup form width (which was set using setter).
+	 *
+	 * @sample
+	 * popupform.width();
+	 *
+	 * @return int
+	 */
 	public int js_width()
 	{
 		return 0;
 	}
 
+	/**
+	 * Set form popup width. If not set, form design width will be used.
+	 *
+	 * @sample
+	 * plugins.window.createFormPopup(forms.orderPicker).width(100).show();
+	 *
+	 * @param width form popup width
+	 *
+	 */
 	public void js_width(int width)
 	{
 
 	}
 
+	/**
+	 * Get the popup form height (which was set using setter).
+	 *
+	 * @sample
+	 * popupform.height();
+	 *
+	 * @return int
+	 */
 	public int js_height()
 	{
 		return 0;
 	}
 
+	/**
+	 * Set form popup height. If not set, form design height will be used.
+	 *
+	 * @sample
+	 * plugins.window.createFormPopup(forms.orderPicker).height(100).show();
+	 *
+	 * @param height form popup height
+	 *
+	 */
 	public void js_height(int height)
 	{
 
 	}
 
+	/**
+	 * Get the popup form x location (which was set using setter).
+	 *
+	 * @sample
+	 * popupform.x();
+	 *
+	 * @return int
+	 */
 	public int js_x()
 	{
 		return 0;
 	}
 
+	/**
+	 * Set form popup x location. The priority sequence for location is: related element, set location, center of screen.
+	 *
+	 * @sample
+	 * plugins.window.createFormPopup(forms.orderPicker).x(100).show();
+	 *
+	 * @param x form popup x location
+	 *
+	 */
 	public void js_x(int x)
 	{
 
 	}
 
+	/**
+	 * Get the popup form y location (which was set using setter).
+	 *
+	 * @sample
+	 * popupform.y();
+	 *
+	 * @return int
+	 */
 	public int js_y()
 	{
 		return 0;
 	}
 
+	/**
+	 * Set form popup y location. The priority sequence for location is: related element, set location, center of screen.
+	 *
+	 * @sample
+	 * plugins.window.createFormPopup(forms.orderPicker).y(100).show();
+	 *
+	 * @param y form popup y location
+	 *
+	 */
 	public void js_y(int y)
 	{
 
 	}
 
+	/**
+	 * Get the popup form show backdrop (which was set using setter).
+	 *
+	 * @sample
+	 * popupform.showBackdrop();
+	 *
+	 * @return boolean
+	 */
 	public boolean js_showBackdrop()
 	{
 		return false;
 	}
 
+
+	/**
+	 * Set whether backdrop will be shown. Default value is false.
+	 *
+	 * @sample
+	 * plugins.window.createFormPopup(forms.orderPicker).showBackdrop(true).show();
+	 *
+	 * @param showBackdrop form popup showBackdrop
+	 *
+	 */
 	public void js_showBackdrop(boolean showBackdrop)
+	{
+
+	}
+
+	/**
+	 * Get the popup form dataprovider (which was set using setter).
+	 *
+	 * @sample
+	 * popupform.dataprovider();
+	 *
+	 * @return String
+	 */
+	public String js_dataprovider()
+	{
+		return null;
+	}
+
+	/**
+	 * Set form popup dataprovider that will be set. If this is set, also scope needs to be specified.
+	 *
+	 * @sample
+	 * plugins.window.createFormPopup(forms.orderPicker).dataprovider('myid').scope(foundset.getSelectedRecord()).show();
+	 *
+	 * @param dataprovider form popup dataprovider
+	 *
+	 */
+	public void js_dataprovider(String dataprovider)
+	{
+
+	}
+
+	/**
+	 * Get the popup form scope (which was set using setter).
+	 *
+	 * @sample
+	 * popupform.scope();
+	 *
+	 * @return Object
+	 */
+	public Object js_scope()
+	{
+		return null;
+	}
+
+	/**
+	 * Set form popup scope that will be modified. If this is set, also dataprovider needs to be specified.
+	 *
+	 * @sample
+	 * plugins.window.createFormPopup(forms.orderPicker).dataprovider('myid').scope(foundset.getSelectedRecord()).show();
+	 *
+	 * @param scope form popup scope to modify
+	 *
+	 */
+	public void js_scope(Object scope)
+	{
+
+	}
+
+	/**
+	 * Show form popup using parameters that were set
+	 *
+	 * @sample
+	 * plugins.window.createFormPopup(forms.orderPicker).x(100).y(100).width(100).height(100).showBackdrop(true).show();
+	 *
+	 *
+	 */
+	public void js_show()
 	{
 
 	}
