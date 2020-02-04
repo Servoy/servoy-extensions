45a485c92587da252ee624eba1cd93ad13a443d6
emera <emera@servoy.com>
2020-02-04 20:41:47 +0200

    SVY-14626 investigate/make a plugin that can make a Json Web Token (JWT) and verify it

---
+++ com.servoy.extensions/src/com/servoy/extensions/plugins/jwt/JWTServer.java
@@ -0,0 +1,192 @@
+/*
+ This file belongs to the Servoy development and deployment environment, Copyright (C) 1997-2020 Servoy BV
+
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Affero General Public License as published by the Free
+ Software Foundation; either version 3 of the License, or (at your option) any
+ later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License along
+ with this program; if not, see http://www.gnu.org/licenses or write to the Free
+ Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+*/
+
+package com.servoy.extensions.plugins.jwt;
+
+import java.rmi.RemoteException;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator.Builder;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.servoy.j2db.plugins.IServerAccess;
+import com.servoy.j2db.plugins.IServerPlugin;
+import com.servoy.j2db.plugins.PluginException;
+import com.servoy.j2db.util.Debug;
+import com.servoy.j2db.util.Utils;
+
+/**
+ * @author emera
+ */
+public class JWTServer implements IServerPlugin, IJWTService
+{
+	private static final String JWT_SECRET_KEY = "jwt.secret.password";
+
+
+	private Properties settings;
+	private JWTVerifier verifier;
+	private Algorithm algorithm;
+
+	public JWTServer()
+	{
+	}
+
+
+	@Override
+	public void load() throws PluginException
+	{
+	}
+
+	@Override
+	public void unload() throws PluginException
+	{
+		settings = null;
+	}
+
+	@Override
+	public Properties getProperties()
+	{
+		Properties props = new Properties();
+		props.put(DISPLAY_NAME, "JWT Plugin"); //$NON-NLS-1$
+		return props;
+	}
+
+	@Override
+	public void initialize(IServerAccess app) throws PluginException
+	{
+		settings = app.getSettings();
+
+		try
+		{
+			app.registerRemoteService(IJWTService.class.getName(), this);
+		}
+		catch (Exception e)
+		{
+			Debug.error(e);
+		}
+
+		Iterator it = settings.keySet().iterator();
+		while (it.hasNext())
+		{
+			String key = (String)it.next();
+			if (key.startsWith("jwt."))
+			{
+				String value = settings.getProperty(key);
+				if (value != null && !value.trim().equals(""))
+				{
+					System.setProperty(key, value);
+				}
+			}
+		}
+	}
+
+	private boolean checkInit()
+	{
+		if (settings.getProperty(JWT_SECRET_KEY) == null)
+		{
+			Debug.error("Cannot create JWT token, the " + JWT_SECRET_KEY + " was not set on the admin page.");
+			return false;
+		}
+		if (algorithm == null)
+		{
+			try
+			{
+				algorithm = Algorithm.HMAC256(settings.getProperty(JWT_SECRET_KEY));
+			}
+			catch (Exception e)
+			{
+				Debug.error(e);
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public Map<String, String> getRequiredPropertyNames()
+	{
+		Map<String, String> req = new LinkedHashMap<String, String>();
+		req.put(JWT_SECRET_KEY,
+			"Shared secret, used to sign and verify the JWT tokens. Should be the same on all servers that want to sign or verify the same tokens.");
+		return req;
+	}
+
+	@Override
+	public String create(JSONObject claims, Date expire) throws RemoteException
+	{
+		try
+		{
+			if (!checkInit()) return null;
+			Builder builder = JWT.create();
+			if (claims != null)
+			{
+				for (String key : claims.keySet())
+				{
+					Object value = claims.get(key);
+					if (value instanceof Boolean) builder.withClaim(key, (Boolean)value);
+					if (value instanceof Date) builder.withClaim(key, (Date)value);
+					if (value instanceof Double) builder.withClaim(key, (Double)value);
+					if (value instanceof Integer) builder.withClaim(key, (Integer)value);
+					if (value instanceof Long) builder.withClaim(key, (Long)value);
+					if (value instanceof String) builder.withClaim(key, (String)value);
+				}
+			}
+			if (expire != null)
+			{
+				builder.withExpiresAt(expire);
+			}
+			return builder.sign(algorithm);
+		}
+		catch (Exception e)
+		{
+			Debug.error("Could not create the JWT token", e);
+		}
+		return null;
+	}
+
+	@Override
+	public JSONObject verify(String token) throws RemoteException
+	{
+		try
+		{
+			if (verifier == null)
+			{
+				if (!checkInit()) return null;
+				verifier = JWT.require(algorithm)
+					.build();
+			}
+			DecodedJWT jwt = verifier.verify(token);
+			String payload = new String(Utils.decodeBASE64(jwt.getPayload()));
+			return new JSONObject(payload);
+		}
+		catch (JWTVerificationException | JSONException e)
+		{
+			Debug.error(e);
+		}
+		return null;
+	}
+}
---
+++ com.servoy.extensions/src/com/servoy/extensions/plugins/jwt/client/JWTProvider.java
@@ -0,0 +1,116 @@
+/*
+ This file belongs to the Servoy development and deployment environment, Copyright (C) 1997-2020 Servoy BV
+
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Affero General Public License as published by the Free
+ Software Foundation; either version 3 of the License, or (at your option) any
+ later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License along
+ with this program; if not, see http://www.gnu.org/licenses or write to the Free
+ Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+*/
+
+package com.servoy.extensions.plugins.jwt.client;
+
+import java.util.Date;
+
+import org.json.JSONObject;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.annotations.JSFunction;
+
+import com.servoy.extensions.plugins.jwt.IJWTService;
+import com.servoy.j2db.documentation.ServoyDocumented;
+import com.servoy.j2db.plugins.IClientPluginAccess;
+import com.servoy.j2db.scripting.IScriptable;
+import com.servoy.j2db.util.Debug;
+
+/**
+ * @author emera
+ */
+@ServoyDocumented(publicName = JWTPlugin.PLUGIN_NAME, scriptingName = "plugins." + JWTPlugin.PLUGIN_NAME)
+public class JWTProvider implements IScriptable
+{
+	private final JWTPlugin plugin;
+	private IJWTService jwtService;
+
+	public JWTProvider(JWTPlugin jwtPlugin)
+	{
+		this.plugin = jwtPlugin;
+	}
+
+	private void createJWTService()
+	{
+		if (jwtService == null)
+		{
+			try
+			{
+				IClientPluginAccess access = plugin.getClientPluginAccess();
+				jwtService = (IJWTService)access.getRemoteService(IJWTService.class.getName());
+			}
+			catch (Exception ex)
+			{
+				Debug.error(ex);
+			}
+		}
+	}
+
+	@JSFunction
+	public String create(Object payload)
+	{
+		return create(payload, null);
+	}
+
+	@JSFunction
+	public String create(Object payload, Date expiresAt)
+	{
+		createJWTService();
+
+		if (jwtService != null)
+		{
+			try
+			{
+				JSONObject obj = new JSONObject();
+				if (payload instanceof Scriptable)
+				{
+					Scriptable scriptable = (Scriptable)payload;
+					for (Object id : scriptable.getIds())
+					{
+						if (id instanceof String)
+						{
+							obj.put((String)id, scriptable.get((String)id, null));
+						}
+					}
+				}
+				return jwtService.create(obj, expiresAt);
+			}
+			catch (Exception e)
+			{
+				Debug.error(e);
+			}
+		}
+		return null;
+	}
+
+	@JSFunction
+	public JSONObject verify(String token)
+	{
+		createJWTService();
+		if (jwtService != null)
+		{
+			try
+			{
+				return jwtService.verify(token);
+			}
+			catch (Exception e)
+			{
+				Debug.error(e);
+			}
+		}
+		return null;
+	}
+}
