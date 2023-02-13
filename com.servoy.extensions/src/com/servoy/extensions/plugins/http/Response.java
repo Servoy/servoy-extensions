2e304d1aab9d4c8fd01320806f64172a7d63a9e0
Johan Compagner <jcompagner@servoy.com>
2023-02-13 16:57:06 +0100

    updated doc

--- com.servoy.extensions/src/com/servoy/extensions/plugins/http/Response.java
+++ com.servoy.extensions/src/com/servoy/extensions/plugins/http/Response.java
@@ -272,7 +272,7 @@ public class Response implements IScriptable, IJavaScriptType
 	 * Needs to be called when not reading content via getResponseBody or getMediaData
 	 * to be able to reuse the client.
 	 * @return true if the entity content is consumed and content stream (if exists) is closed
-	 * @deprecated no longer needed, make sure to call client.close()
+	 * @deprecated no longer needed, make sure to call client.close() when you don't use the client anymore which was used to create this Response
 	 */
 	@Deprecated
 	public boolean js_close()
